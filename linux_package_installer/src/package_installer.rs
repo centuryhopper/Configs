3768e8549be88b286980733a2413d78df1967733
Leo Zhang <43508492+leozhang1@users.noreply.github.com>
2023-08-25 12:36:57 -0400

    added more code to package installer

--- linux_package_installer/src/package_installer.rs
+++ linux_package_installer/src/package_installer.rs
@@ -1,4 +1,4 @@
-use std::process::Command;
+use std::{process::Command, fs::OpenOptions, io::Write};
 
 pub trait PackageInstaller<'a> {
     fn install_packages<T>(&self, iterable: T)
@@ -8,15 +8,26 @@ pub trait PackageInstaller<'a> {
     fn run_bash_command(&self, cmd: &'a str);
 
     fn get_user_input(&self, input: &'a str) -> String;
+
+    fn append_contents_to_file(&self, contents:&'a str, file: &'a str);
 }
 
-pub struct ArchHyperlandInstaller;
+pub struct ArchHyperlandInstaller<'a>
+{
+    instlog_path: &'a str
+}
 
-impl ArchHyperlandInstaller {
-    pub fn new() -> Self {
-        let o = ArchHyperlandInstaller{};
-        o.run_bash_command("yay -Syu --noconfirm");
+impl<'a> ArchHyperlandInstaller<'a> {
+    pub fn new(instlog_path : &'a str) -> Self {
+        let o = ArchHyperlandInstaller{
+            instlog_path: instlog_path
+        };
+        o.run_bash_command("sudo pacman -Syu --noconfirm");
         o
+    }
+
+    pub fn copy_configs(source:&'a str, dest: &'a str) {
+        
     }
 
     pub fn check_for_yay(&self) {}
@@ -26,19 +37,21 @@ impl ArchHyperlandInstaller {
     pub fn clean_out_other_portals(&self) {}
 }
 
-impl<'a> PackageInstaller<'a> for ArchHyperlandInstaller {
+impl<'a> PackageInstaller<'a> for ArchHyperlandInstaller<'a> {
     fn install_packages<T>(&self, packages: T)
     where
         T: Iterator<Item = &'a str>,
     {
         for package in packages {
-            Command::new("yay")
-                .arg("-S")
-                .arg(package)
-                .arg("--needed")
-                .arg("--noconfirm")
-                .output()
-                .expect(&format!("couldn't install package: {:#?}", package));
+            // Command::new("yay")
+            //     .arg("-S")
+            //     .arg(package)
+            //     .arg("--needed")
+            //     .arg("--noconfirm")
+            //     .output()
+            //     .expect(&format!("couldn't install package: {:#?}", package));
+
+            &self.run_bash_command(&format!("yay -S {} --needed --noconfirm", package));
         }
     }
 
@@ -47,15 +60,30 @@ impl<'a> PackageInstaller<'a> for ArchHyperlandInstaller {
 
         println!("parts: {:#?}", parts);
 
-        let status = Command::new(&parts[0])
+        let output = Command::new(&parts[0])
         .args(&parts[1..])
-        .status()
+        .output()
         .expect(&format!("failed to run command: {:#?}", cmd));
 
-        println!("{:?}", status)
+        println!("{:?}", output);
+
+        // Write the combined stdout and stderr to the log file
+        let mut log_file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(&self.instlog_path)
+        .expect("Failed to open log file");
+
+        log_file
+        .write_all(&output.stdout)
+        .expect("Failed to write to log file");
+
+        log_file
+        .write_all(&output.stderr)
+        .expect("Failed to write to log file");
     }
 
-    fn get_user_input(&self, msg: &'a str) -> String{
+    fn get_user_input(&self, msg: &'a str) -> String {
         // Create a new instance of std::io::Stdin
         let stdin = std::io::stdin();
 
@@ -74,5 +102,21 @@ impl<'a> PackageInstaller<'a> for ArchHyperlandInstaller {
         // println!("{}", input == "\n");
 
         input
+    }
+
+    fn append_contents_to_file(&self, contents:&'a str, file: &'a str) {
+
+        println!("{} will be created if it hasn't been already.", file);
+
+        let mut fp = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(file)
+        .expect(&format!("Failed to open file: {}", file));
+
+        fp
+        .write_all(contents.as_bytes())
+        .expect(&format!("Failed to write to file: {}", file));
+        
     }
 }
