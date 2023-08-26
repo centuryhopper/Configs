670e31d073bddeb2109fd1b402495e7c0234be12
Leo Zhang <43508492+leozhang1@users.noreply.github.com>
2023-08-26 08:05:19 -0400

    update

--- linux_package_installer/src/main.rs
+++ linux_package_installer/src/main.rs
@@ -1,202 +1,31 @@
 mod package_installer;
-use std::{path::Path, thread, time::Duration, env, vec};
-use colored::Colorize;
-use indoc::indoc;
-use package_installer::{ArchHyperlandInstaller, PackageInstaller};
+use std::path::Path;
 use dirs::home_dir;
+use package_installer::ArchHyperlandInstaller;
+use package_installer::PackageInstaller;
+
 
 fn main() {
-    println!("home: {:?}", home_dir().unwrap());
     let home = home_dir().unwrap();
-
-    let ai = ArchHyperlandInstaller::new("install.log");
-
-    let expectations = indoc! {
-        "
-        NOTE - You are about to execute a script that would attempt to setup Hyprland.
-        Please note that Hyprland is still in Beta.
-        Please note that VMs are not fully supported and if you try to run this on
-        a Virtual Machine there is a high chance this will fail.
-        Please note that support for Nvidia GPUs is limited and may require
-        more work which is beyond the scope of this script.
-        \n
-        "
-    };
-
-    println!("{}", expectations.yellow());
-    let mut prompt = format!("{}", "ACTION - Would you like to continue with the install (y,n)".yellow());
-    let mut response = ai.get_user_input(prompt.as_str());
-
-    if response.to_lowercase() == "y" {
-        println!("{}", "starting install script...".white());
-    }
-    else {
-        println!("{}", "This script will now exit. No changes were made to your system.".white());
+    let home_path = Path::new(&home);
+    let desired_dir = home_path.join(Path::new("Configs/arch_linux_hyprland/dotfiles"));
+    
+    if !desired_dir.exists() {
+        println!("Please make sure to copy my github repo into your home folder");
         std::process::exit(0);
     }
 
-    let mut notice = indoc! {
-        "NOTE - This script will run some commands that require sudo. You will be prompted to enter your password.
-        If you are worried about entering your password then you may want to review the content of the script."
-    };
+    std::env::set_current_dir(&desired_dir).expect("failed to set to new directory");
 
-    println!("{}", notice.yellow());
+    println!("{:?}", std::env::current_dir().unwrap());
 
-    thread::sleep(Duration::from_secs(3));
+    // let status = Command::new("echo")
+    //     .arg("$USER")
+    //     .status()
+    //     .expect("failed to run command");
 
-    // check for yay. Install it if it doesn't exist
-    if Path::new("/sbin/yay").exists() {
-        println!("{}", "yay was located, moving on.".white());
-    }
-    else {
-        println!("{}", "yay not was located.".white());
-        response = ai.get_user_input(&format!("ACTION - Would you like to install yay (y,n)"));
+    // println!("output: {:?}", status);
+    let ai = ArchHyperlandInstaller::new("install.log");
+    ai.run();
 
-        if response.to_lowercase() == "y" {
-            ai.run_bash_command("git clone https://aur.archlinux.org/yay.git");
-            ai.run_bash_command("cd yay");
-            ai.run_bash_command("makepkg -si --noconfirm");
-            ai.run_bash_command("cd ..");
-        }
-        else {
-            println!("Yay is required for this script, now exiting.");
-            std::process::exit(0);
-        }
-    }
-
-
-    response = ai.get_user_input("Would you like to disable WiFi powersave? (y,n)");
-
-    if response.to_lowercase() == "y" {
-        ai.append_contents_to_file("[connection]\nwifi.powersave = 2", "/etc/NetworkManager/conf.d/wifi-powersave.conf");
-        println!("{}", "NOTE - Restarting NetworkManager service...".yellow());
-
-        thread::sleep(Duration::from_secs(1));
-        ai.run_bash_command("sudo systemctl restart NetworkManager");
-        thread::sleep(Duration::from_secs(3));
-    }
-
-    let main_components: Vec<&str> = vec![
-        "hyprland",
-        "kitty",
-        "waybar",
-        "swww",
-        "swaylock-effects",
-        "wofi",
-        "wlogout",
-        "mako",
-        "xdg-desktop-portal-hyprland",
-        "swappy",
-        "grim",
-        "slurp",
-        "thunar",
-    ];
-
-    let additional_components: Vec<&str> = vec![
-        "polkit-gnome",
-        "python-requests",
-        "pamixer",
-        "pavucontrol",
-        "brightnessctl",
-        "bluez",
-        "bluez-utils",
-        "blueman",
-        "network-manager-applet",
-        "gvfs",
-        "thunar-archive-plugin",
-        "file-roller",
-        "btop",
-        "pacman-contrib",
-    ];
-
-    let theme_components: Vec<&str> = vec![
-        "starship",
-        "ttf-jetbrains-mono-nerd",
-        "noto-fonts-emoji",
-        "lxappearance",
-        "xfce4-settings",
-        "sddm-git",
-        "qt5-svg",
-        "qt5-quickcontrols2",
-        "qt5-graphicaleffects",
-    ];
-    response = ai.get_user_input(&format!("Would you like to install the packages (y,n)").yellow());
-
-    if response.to_lowercase() == "y" {
-        println!("Updating yay database...");
-        ai.run_bash_command("yay -Syu --noconfirm");
-
-        println!("{}", "Installing main components. This may take a while...".yellow());
-
-        ai.install_packages(main_components.into_iter());
-
-        ai.install_packages(additional_components.into_iter());
-
-        ai.install_packages(theme_components.into_iter());
-
-
-    }
-
-    let servicesToEnable = vec![
-        "sudo systemctl enable --now bluetooth.service",
-        "sudo systemctl enable sddm",
-    ];
-
-    for service in servicesToEnable {
-        ai.run_bash_command(service);
-        thread::sleep(Duration::from_secs(2));
-    }
-
-    println!("{}", "Clearning out conflicting xdg portals...".yellow());
-    ai.run_bash_command("yay -R --noconfirm xdg-desktop-portal-gnome xdg-desktop-portal-gtk");
-
-    
-
-    // let configs = vec![
-    //     "fish",
-    //     "hypr",
-    //     "kitty",
-    //     "lf",
-    //     "mako",
-    //     "nvim",
-    //     "shaders",
-    //     "tmux",
-    //     "Wallpapers",
-    //     "waybar",
-    //     "wlogout",
-    //     "wofi",
-    // ];
-
-    // // ai.run_bash_command("cp /home/leo_zhang/Documents/GitHub/Configs/arch_linux_hyprland/dotfiles/starship.toml /home/leo_zhang/.config/test/");
-    // // copy starship
-    // ai.run_bash_command("");
-
-    // // copy bashrc
-    // ai.run_bash_command("");
-
-    // copy over configs
-
-    // set some scripts as executable
-    let exes = vec![
-        format!("chmod +x {}/.config/hypr/scripts/bgaction", home.to_string_lossy()),
-        format!("chmod +x {}/.config/hypr/scripts/xdg-portal-hyprland", home.to_string_lossy()),
-        format!("chmod +x {}/.config/waybar/scripts/waybar-wttr.py", home.to_string_lossy()),
-        format!("chmod +x {}/.config/waybar/scripts/baraction", home.to_string_lossy()),
-        format!("chmod +x {}/.config/waybar/scripts/update-sys", home.to_string_lossy()),
-    ];
-
-    for ex in exes {
-        ai.run_bash_command(ex.as_str());
-    }
-
-    // copy the sddm theme
-
-    // setup the first look and feel as dark
-
-    // let response = ai.get_user_input("Would you like to start Hyprland now? (y,n)");
-
-    // start hyprland if user says yes
-    // if (response.to_lowercase() == "y") {
-    //     println!("sudo systemctl start sddm");
-    // }
 }
--- linux_package_installer/src/package_installer.rs
+++ linux_package_installer/src/package_installer.rs
@@ -1,4 +1,7 @@
-use std::{process::Command, fs::OpenOptions, io::Write};
+use std::{fs::OpenOptions, io::Write, process::Command, thread, time::Duration, path::Path};
+use indoc::indoc;
+use colored::Colorize;
+
 
 pub trait PackageInstaller<'a> {
     fn install_packages<T>(&self, iterable: T)
@@ -9,32 +12,23 @@ pub trait PackageInstaller<'a> {
 
     fn get_user_input(&self, input: &'a str) -> String;
 
-    fn append_contents_to_file(&self, contents:&'a str, file: &'a str);
+    fn append_contents_to_file(&self, contents: &'a str, file: &'a str);
+
+    fn run(&self);
 }
 
-pub struct ArchHyperlandInstaller<'a>
-{
-    instlog_path: &'a str
+pub struct ArchHyperlandInstaller<'a> {
+    instlog_path: &'a str,
 }
 
 impl<'a> ArchHyperlandInstaller<'a> {
-    pub fn new(instlog_path : &'a str) -> Self {
-        let o = ArchHyperlandInstaller{
-            instlog_path: instlog_path
+    pub fn new(instlog_path: &'a str) -> Self {
+        let o = ArchHyperlandInstaller {
+            instlog_path: instlog_path,
         };
         o.run_bash_command("sudo pacman -Syu --noconfirm");
         o
     }
-
-    pub fn copy_configs(source:&'a str, dest: &'a str) {
-        
-    }
-
-    pub fn check_for_yay(&self) {}
-
-    pub fn disable_wifi_powersave_mode(&self) {}
-
-    pub fn clean_out_other_portals(&self) {}
 }
 
 impl<'a> PackageInstaller<'a> for ArchHyperlandInstaller<'a> {
@@ -51,36 +45,36 @@ impl<'a> PackageInstaller<'a> for ArchHyperlandInstaller<'a> {
             //     .output()
             //     .expect(&format!("couldn't install package: {:#?}", package));
 
-            &self.run_bash_command(&format!("yay -S {} --needed --noconfirm", package));
+            self.run_bash_command(&format!("yay -S {} --needed --noconfirm", package));
         }
     }
 
     fn run_bash_command(&self, cmd: &'a str) {
-        let parts : Vec<&str> = cmd.split_whitespace().collect::<Vec<_>>();
+        let parts: Vec<&str> = cmd.split_whitespace().collect::<Vec<_>>();
 
         println!("parts: {:#?}", parts);
 
         let output = Command::new(&parts[0])
-        .args(&parts[1..])
-        .output()
-        .expect(&format!("failed to run command: {:#?}", cmd));
+            .args(&parts[1..])
+            .output()
+            .expect(&format!("failed to run command: {:#?}", cmd));
 
         println!("{:?}", output);
 
         // Write the combined stdout and stderr to the log file
         let mut log_file = OpenOptions::new()
-        .create(true)
-        .append(true)
-        .open(&self.instlog_path)
-        .expect("Failed to open log file");
+            .create(true)
+            .append(true)
+            .open(self.instlog_path)
+            .expect("Failed to open log file");
 
         log_file
-        .write_all(&output.stdout)
-        .expect("Failed to write to log file");
+            .write_all(&output.stdout)
+            .expect("Failed to write to log file");
 
         log_file
-        .write_all(&output.stderr)
-        .expect("Failed to write to log file");
+            .write_all(&output.stderr)
+            .expect("Failed to write to log file");
     }
 
     fn get_user_input(&self, msg: &'a str) -> String {
@@ -95,28 +89,178 @@ impl<'a> PackageInstaller<'a> for ArchHyperlandInstaller<'a> {
 
         // Read the user input into the String
         stdin.read_line(&mut input).expect("Failed to read line");
-        
+
         // Print the user's input
         println!("You entered: {}", input);
-        
+
         // println!("{}", input == "\n");
 
         input
     }
 
-    fn append_contents_to_file(&self, contents:&'a str, file: &'a str) {
-
+    fn append_contents_to_file(&self, contents: &'a str, file: &'a str) {
         println!("{} will be created if it hasn't been already.", file);
 
         let mut fp = OpenOptions::new()
-        .create(true)
-        .append(true)
-        .open(file)
-        .expect(&format!("Failed to open file: {}", file));
+            .create(true)
+            .append(true)
+            .open(file)
+            .expect(&format!("Failed to open file: {}", file));
 
-        fp
-        .write_all(contents.as_bytes())
-        .expect(&format!("Failed to write to file: {}", file));
-        
+        fp.write_all(contents.as_bytes())
+            .expect(&format!("Failed to write to file: {}", file));
+    }
+
+    fn run(&self) {
+        let expectations = indoc! {
+            "
+        NOTE - You are about to execute a script that would attempt to setup Hyprland.
+        Please note that Hyprland is still in Beta.
+        Please note that VMs are not fully supported and if you try to run this on
+        a Virtual Machine there is a high chance this will fail.
+        Please note that support for Nvidia GPUs is limited and may require
+        more work which is beyond the scope of this script.
+        \n
+        "
+        };
+
+        println!("{}", expectations.yellow());
+        let mut prompt = format!(
+            "{}",
+            "ACTION - Would you like to continue with the install (y,n)".yellow()
+        );
+
+        let mut response = self.get_user_input(prompt.as_str());
+
+        if response.to_lowercase() == "y" {
+            println!("{}", "starting install script...".white());
+        } else {
+            println!(
+                "{}",
+                "This script will now exit. No changes were made to your system.".white()
+            );
+            std::process::exit(0);
+        }
+
+        let notice = indoc! {
+            "NOTE - This script will run some commands that require sudo. You will be prompted to enter your password.
+            If you are worried about entering your password then you may want to review the content of the script."
+        };
+
+        println!("{}", notice.yellow());
+
+        thread::sleep(Duration::from_secs(3));
+
+        // check for yay. Install it if it doesn't exist
+        if Path::new("/sbin/yay").exists() {
+            println!("{}", "yay was located, moving on.".white());
+        } else {
+            println!("{}", "yay not was located.".white());
+            prompt = format!("{}", "ACTION - Would you like to install yay (y,n)".yellow());
+            response = self.get_user_input(&prompt);
+
+            if response.to_lowercase() == "y" {
+                self.run_bash_command("git clone https://aur.archlinux.org/yay.git");
+                self.run_bash_command("cd yay");
+                self.run_bash_command("makepkg -si --noconfirm");
+                self.run_bash_command("cd ..");
+            } else {
+                println!("Yay is required for this script, now exiting.");
+                std::process::exit(0);
+            }
+        }
+
+        response = self.get_user_input("Would you like to disable WiFi powersave? (y,n)");
+
+        if response.to_lowercase() == "y" {
+            self.append_contents_to_file(
+                "[connection]\nwifi.powersave = 2",
+                "/etc/NetworkManager/conf.d/wifi-powersave.conf",
+            );
+            println!("{}", "NOTE - Restarting NetworkManager service...".yellow());
+
+            thread::sleep(Duration::from_secs(1));
+            self.run_bash_command("sudo systemctl restart NetworkManager");
+            thread::sleep(Duration::from_secs(3));
+        }
+
+        let main_components: Vec<&str> = vec![
+            "hyprland",
+            "kitty",
+            "waybar",
+            "swww",
+            "swaylock-effects",
+            "wofi",
+            "wlogout",
+            "mako",
+            "xdg-desktop-portal-hyprland",
+            "swappy",
+            "grim",
+            "slurp",
+            "thunar",
+        ];
+
+        let additional_components: Vec<&str> = vec![
+            "polkit-gnome",
+            "python-requests",
+            "pamixer",
+            "pavucontrol",
+            "brightnessctl",
+            "bluez",
+            "bluez-utils",
+            "blueman",
+            "network-manager-applet",
+            "gvfs",
+            "thunar-archive-plugin",
+            "file-roller",
+            "btop",
+            "pacman-contrib",
+        ];
+
+        let theme_components: Vec<&str> = vec![
+            "starship",
+            "ttf-jetbrains-mono-nerd",
+            "noto-fonts-emoji",
+            "lxappearance",
+            "xfce4-settings",
+            "sddm-git",
+            "qt5-svg",
+            "qt5-quickcontrols2",
+            "qt5-graphicaleffects",
+        ];
+        response =
+            self.get_user_input(&format!("Would you like to install the packages (y,n)").yellow());
+
+        if response.to_lowercase() == "y" {
+            println!("Updating yay database...");
+            self.run_bash_command("yay -Syu --noconfirm");
+
+            println!(
+                "{}",
+                "Installing main components. This may take a while...".yellow()
+            );
+
+            self.install_packages(main_components.into_iter());
+
+            self.install_packages(additional_components.into_iter());
+
+            self.install_packages(theme_components.into_iter());
+        }
+
+        let services = vec![
+            "sudo systemctl enable --now bluetooth.service",
+            "sudo systemctl enable sddm",
+        ];
+
+        for service in services {
+            self.run_bash_command(service);
+            thread::sleep(Duration::from_secs(2));
+        }
+
+        println!("{}", "Clearning out conflicting xdg portals...".yellow());
+        self.run_bash_command("yay -R --noconfirm xdg-desktop-portal-gnome xdg-desktop-portal-gtk");
+
+        // copy configs over and finish remaining setup
+        self.run_bash_command("bash set-configs");
     }
 }
